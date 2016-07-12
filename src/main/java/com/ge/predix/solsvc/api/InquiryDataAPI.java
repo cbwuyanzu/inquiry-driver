ba682d63d884f566b21e4c49a3748ecec3cf03e6
cbwuyanzu <277850402@qq.com>
2016-07-12 11:05:45 -0400

    fist

---
+++ src/main/java/com/ge/predix/solsvc/api/InquiryDataAPI.java
@@ -0,0 +1,93 @@
+package com.ge.predix.solsvc.api;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author predix -
+ */
+@Consumes(
+{
+        "application/json", "application/xml"
+})
+@Produces(
+{
+        "application/json", "application/xml"
+})
+@Path("/driver")
+public interface InquiryDataAPI
+{
+	/**
+	 * @return -
+	 */
+	@GET
+	@Path("/ping")
+	public Response greetings();
+
+	/**
+	 * @param id
+	 *            -
+	 * @param authorization
+	 *            -
+	 * @param starttime
+	 *            -
+	 * @param tagLimit -
+	 * @param tagorder -
+	 * @return -
+	 */
+	@GET
+	@Path("/yearly_data/{id}")
+	public Response getYearlyDataPoints(@PathParam("id") String id,
+			@HeaderParam(value = "Authorization") String authorization,
+			@DefaultValue("1y-ago") @QueryParam("starttime") String starttime,
+			@DefaultValue("10000") @QueryParam("taglimit") String tagLimit,@DefaultValue("asc") @QueryParam("order") String tagorder);
+	/**
+	 * @param id
+	 *            -
+	 * @param authorization
+	 *            -
+	 * @param starttime
+	 *            -
+	 * @param tagLimit -
+	 * @param tagorder -
+	 * @return -
+	 */
+	@GET
+	@Path("/weekly_data/{id}")
+	public Response getWeeklyDataPoints(@PathParam("id") String id,
+			@HeaderParam(value = "Authorization") String authorization,
+			@DefaultValue("1w-ago") @QueryParam("starttime") String starttime,
+			@DefaultValue("10000") @QueryParam("taglimit") String tagLimit,@DefaultValue("asc") @QueryParam("order") String tagorder);
+
+	/**
+	 * @param id
+	 *            -
+	 * @param authorization
+	 *            -
+	 * @return -
+	 */
+	@GET
+	@Path("/latest_data/{id}")
+	public Response getLatestDataPoints(@PathParam("id") String id,
+			@HeaderParam(value = "authorization") String authorization);
+
+	/**
+	 *
+	 * @return List of tags
+	 */
+	@GET
+	@Path("/tags")
+	public Response getDataTags();
+	/**
+	 *
+	 * @return List of tags
+	 */
+}
